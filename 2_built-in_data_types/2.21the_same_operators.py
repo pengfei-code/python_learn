690bc58a4481979e3988003328ea2dd48afc140d
pengfei-code <spf_tay@163.com>
2020-12-25 07:23:35 +0800

    Update 2.21the_same_operators.py

--- 2_built-in_data_types/2.21the_same_operators.py
+++ 2_built-in_data_types/2.21the_same_operators.py
@@ -1,11 +1,11 @@
 # the same operators is is not
 
 #what is the difference between == and is
-# In the python the comparison of the equal sign is the value
-#but the “is” is used to determine whether the two object are refrerenced
-#by the  variables is the same
-##in the python3 ， the digital existenc of cache ，so the defined numbers  can be reused
-#but in the python2 ,the scope of cache of a number is between -5 to 255
+# == is equals to value
+
+# "is" is equals to id
+
+
 a = 2E1000
 b = 2E1000
 print("a == b:",a==b)
